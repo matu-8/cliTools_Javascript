de7c7177f9e02d776bba2963add4192b5b02c85c
Matias Agüero <agueromatias214@gmail.com>
2025-12-30 13:21:15 -0300

    Inicio de proyecto automatizacion de configuracion para proyectos

---
+++ index.js
@@ -0,0 +1,10 @@
+#!/usr/bin/env node //*Esto es una shebang, es una linea que indica a sistema operativo que herramienta utilizar para ejecutar el archivo
+
+//*importacion del modulo de file system de node.
+import * as fs from 'node:fs';
+
+//Creo un objeto donde se define la estructura de archivos
+const projectStructure = {
+    src:['index.js'],
+    public:['index.html', 'style.css']
+};

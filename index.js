e57d9127bfe29672750acf14356e9e0e36dded5c
Matias Agüero <agueromatias214@gmail.com>
2026-01-01 14:57:30 -0300

    Ultimos cambios: Se finaliza la herrmieta, pero hay que solucionar un problema de ejecucion de la misma. Añadida una linea para ejecución de la herramienta en package.json,

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
-#!/usr/bin/env node //*Esto es una shebang, es una linea que indica a sistema operativo que herramienta utilizar para ejecutar el archivo
+#!/usr/bin/env node 
+//Esto es una shebang, es una linea que indica a sistema operativo que herramienta utilizar para ejecutar el archivo
 
-//*importacion del modulo de file system de node.
-import * as fs from 'node:fs';
-
+//importacion del modulo de file system de node.
+import * as fs from "node:fs";
 //Creo un objeto donde se define la estructura de archivos
 const projectStructure = {
     'src':['index.js'],
     'public':['index.html', 'style.css']
 };
-const structureFileArray = Object.entries(projectStructure)
+const structureFileArray = Object.entries(projectStructure);
+
 structureFileArray.forEach(([dir, files])=>{
     fs.mkdirSync(dir, {recursive:true})
+    files.forEach((file)=>fs.writeFileSync(`${dir}/${file}`,''));
 });
+console.log(`La estructura del proyecto se creó con exito`)
